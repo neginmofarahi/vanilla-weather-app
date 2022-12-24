8da60d1e277df0106ff84bfada58a6fe40eb4a7b
Negin Mofarahi <neginmofarahi@yahoo.com>
2022-12-24 14:18:21 +0330

    api fixing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 function displayTemp(response) {
-  console.log(response.data.main.temp);
+  let strong = document.querySelector("strong");
+  let temp = Math.round(response.data.main.temp);
+  strong.innerHTML = temp;
 }
 let apiKey = "7d5433d322af5ac78f642274f8113911";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=shiraz&appid=${apiKey}&units=metric`;
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=${apiKey}&units=metric`;
 
 axios.get(apiURL).then(displayTemp);
