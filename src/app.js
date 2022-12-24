8e7013a359f241e50de16b2637b20f231b179beb
Negin Mofarahi <neginmofarahi@yahoo.com>
2022-12-24 19:45:45 +0330

    searched city apear in h1

--- src/app.js
+++ src/app.js
@@ -8,16 +8,17 @@ function displayTemp(response) {
   wind.innerHTML = Math.round(response.data.wind.speed);
 }
 
-/*function search(event) {
+function search(event) {
   event.preventDefault();
   let h1 = document.querySelector("h1");
   let enteredCity = document.querySelector("#search-city");
   h1.innerHTML = enteredCity.value;
 }
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);*/
+form.addEventListener("submit", search);
 
 let apiKey = "7d5433d322af5ac78f642274f8113911";
+//let form;
 let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=${apiKey}&units=metric`;
 console.log(apiURL);
 
